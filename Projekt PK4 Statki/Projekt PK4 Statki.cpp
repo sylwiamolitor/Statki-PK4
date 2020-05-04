a83d080331cc61479db7d8b59d0b1153930ffdec
sylwiamolitor <43808701+sylwiamolitor@users.noreply.github.com>
2020-05-04 20:41:57 +0200

    Update Projekt PK4 Statki.cpp

--- Projekt PK4 Statki/Projekt PK4 Statki.cpp
+++ Projekt PK4 Statki/Projekt PK4 Statki.cpp
@@ -6,22 +6,83 @@
 #include "GraphicObject.h"
 #include "GameLogic.h"
 #include <Windows.h>
-
+#include <list>
+#include <fstream>
 
 //lista do zapisu
-
+std::list<Button>ButtonList;
 
 
 
 int main()
 {
 
-	sf::RenderWindow window(sf::VideoMode(1880, 1000), "Statki!");//, sf::Style::Fullscreen);
-	Create_Board mine_board;
+	std::ifstream plikWej("Zapis_Rozgrywki.txt");
+	
+	Create_Board my_board;
 	Create_Board enemy_board;
 
-	//rectangle.setPosition(200.f, 10.f); // i ustawiamy mu pozycję
-	//sf::CircleShape shape(100.f);
+	if (plikWej)
+	{
+		//dodac tablice zestrzeleń
+		std::string linijka;
+		for (int i = 0; i < 10; i++)
+		{
+			
+			std::getline(plikWej, linijka);
+			for (int j = 0; j < 10; j++)
+			{
+				if(linijka[j] == '1')
+					my_board.set_ship(i, j);
+			}//wczytuj do planszy swojej
+		}
+		std::getline(plikWej, linijka);
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			std::getline(plikWej, linijka);
+			for (int j = 0; j < 10; j++)
+			{
+				if (linijka[j] == '1')
+					my_board.shoot_ship(i, j);
+			}//wczytuj do planszy swojej
+		}
+		std::getline(plikWej, linijka);
+
+
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			std::getline(plikWej, linijka);
+			for (int j = 0; j < 10; j++)
+			{
+				if (linijka[j] == '1')
+					enemy_board.set_ship(i, j);
+			}		
+		}
+
+		std::getline(plikWej, linijka);
+
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			std::getline(plikWej, linijka);
+			for (int j = 0; j < 10; j++)
+			{
+				if (linijka[j] == '1')
+					enemy_board.shoot_ship(i, j);
+			}
+		}
+
+		plikWej.close();
+	}
+
+	sf::RenderWindow window(sf::VideoMode(1880, 1000), "Statki!");//, sf::Style::Fullscreen);
+	
+
 	// O - trafiony , X puste, . nic
 	//rozgrywka ty-komputer - komputer strzela randem
 
@@ -34,17 +95,22 @@ int main()
 	{
 		// error...
 	}
-
 	
-	//odczyt z o=pliku
-
 	Button przycisk_wyjscia("EXIT", font, 40, sf::Vector2f(1100, 100));
 	Button przycisk_nowa_rozgrywka("NOWA ROZGRYWKA", font, 40, sf::Vector2f(1100, 300));
 	Button przycisk_wynik_przeciwnika("WYNIK PRZECIWNIKA:", font, 40, sf::Vector2f(1100, 500));
 	Button przycisk_twoj_wynik("TWOJ WYNIK:", font, 40, sf::Vector2f(1100, 700));
+	
+	ButtonList.push_back(przycisk_nowa_rozgrywka);
+	ButtonList.push_back(przycisk_twoj_wynik);
+	ButtonList.push_back(przycisk_wyjscia);
+	ButtonList.push_back(przycisk_wynik_przeciwnika);
+
+	
+	
 	Board plansza1(font, sf::Vector2f(0, 0));
 	Board plansza2(font, sf::Vector2f(0, 500));
-	// ... while
+	
 	sf::Event event;
 	sf::Mouse mouse;
 	while (window.isOpen())
@@ -60,99 +126,96 @@ int main()
 				window.close();
 			if (mouse.isButtonPressed(sf::Mouse::Left))
 			{
-			mouse_x = mouse.getPosition(window).x;
-			mouse_y = mouse.getPosition(window).y;
+				mouse_x = mouse.getPosition(window).x;
+				mouse_y = mouse.getPosition(window).y;
+			
+				if (mouse_x > 1100 && mouse_y > 100 && mouse_x < 1600 && mouse_y < 200) //na sztywno
+					window.close();
+					
 			}
-			if (mouse_x > 1100 && mouse_y > 100 && mouse_x < 1600 && mouse_y < 200) //na sztywno
-				window.close();
-
-		 
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) //wciśnięto klawisz ESC
+				window.close(); //zakończ aplikację
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) //wciśnięto klawisz ESC
-			window.close(); //zakończ aplikację
+		
 		
 		window.draw(plansza1);
 		window.draw(plansza2);
-		window.draw(przycisk_wyjscia);
-		window.draw(przycisk_nowa_rozgrywka);
-		window.draw(przycisk_twoj_wynik);
-		window.draw(przycisk_wynik_przeciwnika);
+		
+		for (Button a : ButtonList)
+		{
+			window.draw(a);
+		}
+		
 		window.display();
 		window.clear();
 	}
 	//komunikat kto wygral i zapis do pliku
 	//dzwieki?
+	
+	std::ofstream plikWyj("Zapis_Rozgrywki.txt");
+	
+	if (plikWyj)
+	{
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			for (int j = 0; j < 10; j++)
+			{
+				if (my_board.is_ship_there(i, j) == 1)
+					plikWyj << 1;
+				else
+					plikWyj << 0;
+			}
+			plikWyj << std::endl;
+		}
+		plikWyj << std::endl;
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			for (int j = 0; j < 10; j++)
+			{
+				if (my_board.is_ship_there_shot(i, j) == 1)
+					plikWyj << 1;
+				else
+					plikWyj << 0;
+			}
+			plikWyj << std::endl;
+		}
+		
+		plikWyj << std::endl;
+
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			for (int j = 0; j < 10; j++)
+			{
+				if (enemy_board.is_ship_there(i, j) == 1)
+					plikWyj << 1;
+				else
+					plikWyj << 0;
+			}
+			plikWyj << std::endl;
+		}
+		plikWyj << std::endl;
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			for (int j = 0; j < 10; j++)
+			{
+				if (enemy_board.is_ship_there_shot(i, j) == 1)
+					plikWyj << 1;
+				else
+					plikWyj << 0;
+			}
+			plikWyj << std::endl;
+		}
+
+		plikWyj.close();
+	}
+	
 	return 0;
 }
-/*
-const std::string fontPath = "Resources/comic.ttf";
-const sf::Vector2u windowDimensions = sf::Vector2u(800, 600);
-
-class Player
-{
-public:
-	Player(sf::Font& font, sf::Vector2f position)
-		: shape(sf::Vector2f(1000.f, 1000.f))
-		, text("DŁUUUUUGI TEEEEKST", font)
-	{
-		text.setFillColor(sf::Color::Blue);
-	}
-
-	void update(sf::Vector2f position)
-	{
-		sf::FloatRect textFrect = text.getGlobalBounds();
-		sf::FloatRect shapeFrect = shape.getGlobalBounds();
-		sf::Vector2f textPosition;
-
-		textPosition.x = shapeFrect.left + (shapeFrect.width - textFrect.width) * 0.5f;
-		textPosition.y = shapeFrect.top + (shapeFrect.height - textFrect.height) * 0.5f;
-
-		shape.setPosition(position);
-		text.setPosition(textPosition);
-	}
-
-	void draw(sf::RenderTarget& target) const
-	{
-		target.draw(shape);
-		target.draw(text);
-	}
-
-private:
-	sf::RectangleShape shape;
-	sf::Text text;
-};
-
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(windowDimensions.x, windowDimensions.y), "Example Application");
-
-	sf::Font font;
-//	if (!font.loadFromFile(fontPath))
-	//{
-	//	std::cout << "Failed to load file at: " << fontPath << std::endl;
-	//	return -1;
-//	}
-
-	Player player(font, sf::Vector2f(300.f, 300.f));
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			switch (event.type)
-			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-			}
-		}
-//player.update(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)));
-
-		window.clear();
-		player.draw(window);
-		window.display();
-	}
-
-	return 0;
-}*/
